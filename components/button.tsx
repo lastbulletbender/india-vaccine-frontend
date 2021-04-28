c0a84a0216b4debfb662bbe73b298c3514b568b1
Abhrajit Chattopadhyay <abhrajit.c@media.net>
2021-04-28 16:44:24 +0530

    Use lowercase for imports
    Linux is case sensitive which is causing builds to fail inside Dockerfile

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 
-import styles from "../styles/Button.module.css"
+import styles from "../styles/button.module.css"
 
 interface ButtonInterface {
     children: ReactNode,
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 
-import styles from "../styles/Footer.module.css"
+import styles from "../styles/footer.module.css"
 
 export default function Footer() {
     return (
